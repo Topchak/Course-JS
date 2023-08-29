25c00a8f0828879a71d7e5e38ba06a0bcd44bc98
Slav <slava.1994@ickoud.com>
2023-08-29 17:38:50 +0300

    add file

---
+++ src/js/condition.js
@@ -0,0 +1,18 @@
+const num = 501;
+
+
+
+switch (num) {
+  case 49:
+    console.log("49");
+    break;
+  case 100:
+    console.log('100');
+    break;
+  case 50:
+    console.log('oh-yeah')
+    break;
+  default:
+    console.log('nothing');
+    break;
+}
